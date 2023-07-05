eb0230cb2dcda827ef9faa25af10e166ad185df8
Jahn Saito <jahn.saito@gmail.com>
2023-07-05 08:29:28 +0200

    Privatise member fields of ApiWrapper class

--- apiwrapper.js
+++ apiwrapper.js
@@ -1,21 +1,27 @@
 class ApiWrapper {
+  #octokit;
+
+  #owner;
+
+  #repository;
+
   constructor({ octokit, owner, repository }) {
-    this.octokit = octokit;
-    this.owner = owner;
-    this.repository = repository;
+    this.#octokit = octokit;
+    this.#owner = owner;
+    this.#repository = repository;
 
     // the value of this string is not documented other than:
     // "A unique identifier for the client performing the mutation."
     // https://docs.github.com/en/graphql/reference/mutations
-    this.clientMutationId = `sync-repository-projects-${this.owner}-${this.repository}`;
+    this.clientMutationId = `sync-repository-projects-${this.#owner}-${this.#repository}`;
   }
 
   async createProject({ title, organization, repository }) {
-    const { createProjectV2: { projectV2: { id } } } = await this.octokit.graphql(`
+    const { createProjectV2: { projectV2: { id } } } = await this.#octokit.graphql(`
       mutation {
         createProjectV2(
           input: {
-            ownerId: "${organization.id}",
+            #ownerId: "${organization.id}",
             title: "${title}",
             repositoryId: "${repository.id}",
           }
@@ -30,10 +36,10 @@ class ApiWrapper {
   }
 
   async fetchOrganiztion() {
-    const { organization } = await this.octokit.graphql(
+    const { organization } = await this.#octokit.graphql(
       `
       query {
-        organization(login: "${this.owner}") {
+        organization(login: "${this.#owner}") {
           id
           name
         }
@@ -51,9 +57,9 @@ class ApiWrapper {
   async fetchRepository({ repositoryName }) {
     // max limit for `first` is 100
     // https://docs.github.com/en/graphql/overview/resource-limitations
-    const response = await this.octokit.graphql.paginate(`
+    const response = await this.#octokit.graphql.paginate(`
       query paginate($cursor: String) {
-        repository(owner: "${this.owner}", name: "${repositoryName}") {
+        repository(#owner: "${this.#owner}", name: "${repositoryName}") {
           name
           id
           projectsV2(first: 100, after: $cursor) {
@@ -72,7 +78,7 @@ class ApiWrapper {
   }
 
   async deleteProject({ project }) {
-    const { projectId: id } = await this.octokit.graphql(`
+    const { projectId: id } = await this.#octokit.graphql(`
       mutation{
         deleteProjectV2(
           input: {
